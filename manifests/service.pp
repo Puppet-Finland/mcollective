9c102c7718e9e423609c721309ac4fcbcca657ec
Samuli Seppänen <samuli@openvpn.net>
2016-07-06 11:30:31 +0000

    Ensure that package upgrades do not re-enable mcollectived on Debian
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- manifests/service.pp
+++ manifests/service.pp
@@ -16,4 +16,19 @@ class mcollective::service
         name   => $::mcollective::params::service_name,
         enable => $enable,
     }
+
+    # Disable the service in /etc/default/mcollective on Debian-based distros.
+    # Without this every mcollective package upgrade will re-enable mcollectived
+    # temporarily. On systems with systemd this is probably not required, but
+    # seems harmless.
+    if $::osfamily == 'Debian' {
+        $default_file = '/etc/default/mcollective'
+
+        ini_setting { 'mcollective-start':
+            ensure  => 'present',
+            path    => $default_file,
+            setting => 'START',
+            value   => bool2str($enable),
+        }
+    }
 }
